7997ba2bfcae0dd278eb6c38851212cc01df3c1d
zenoxygen <zenoxygen@protonmail.com>
2019-11-03 17:41:53 +0100

    Fix comments in main example file

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ extern crate bayespam;
 use bayespam::classifier;
 
 fn main() -> Result<(), std::io::Error> {
-    // Classify a typical spam message
+    // Identify a typical spam message
     let spam = "Lose up to 19% weight. Special promotion on our new weightloss.";
     let score = classifier::score(spam)?;
     let is_spam = classifier::identify(spam)?;
     println!("{:.4?}", score);
     println!("{:?}", is_spam);
 
-    // Classify a typical ham message
+    // Identify a typical ham message
     let ham = "Hi Bob, can you send me your machine learning homework?";
     let score = classifier::score(ham)?;
     let is_spam = classifier::identify(ham)?;
